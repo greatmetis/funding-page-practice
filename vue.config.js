49d32c31853981c5298d8e8c4585e98f1c46c45b
Metis Teng <greatmetis@gmail.com>
2021-10-29 15:14:30 +1000

    fix config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-<<<<<<< HEAD
     ? '/funding-page-practice/'
-=======
-    ? 'funding-page-practice'
->>>>>>> 10f63232eaead2d7fa17fa4440764adfe10d7d2a
     : '/'
 }
